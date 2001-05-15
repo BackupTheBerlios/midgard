dd611062c383a3459ca3f52572496a92415b8292
thoma <thoma>
2001-05-15 15:35:37 +0000

    *** empty log message ***

---
+++ midgard/src/midgard_CG_typ_2.cc
@@ -0,0 +1,31 @@
+// $Id: midgard_CG_typ_2.cc,v 1.1 2001/05/15 15:35:37 thoma Exp $
+/*  Midgard Character Generator
+ *  Copyright (C) 2001 Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+                
+                
+#include "midgard_CG.hh"
+
+bool midgard_CG::get_typ_s(const string& mod,const styp& t)
+{
+  if (mod=="Magie")
+   {
+     if (t.z=="z" || t.z=="j") return true;
+     else return false;
+   }
+  else abort();
+}
