ad4147cee4f9708c341d62eedf4d6ec3ed5d1a24
christof <christof>
2001-11-14 08:19:43 +0000

    typ_std2mcg, wegen As->Ass, Or->Ord

---
+++ midgard/xml/import_common.h
@@ -0,0 +1,26 @@
+// $Id: import_common.h,v 1.1 2001/11/14 08:19:43 christof Exp $
+/*  Midgard Roleplaying Character Generator
+ *  Copyright (C) 2001 Christof Petig
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <string>
+
+static std::string typ_std2mcg(const std::string &t)
+{  if (t=="Or") return "Ord";
+   else if (t=="As") return "Ass";
+   else return t;
+}
