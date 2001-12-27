c567b771e441d1520e592a5ebb5b41f0735d5495
christof <christof>
2001-12-27 21:20:41 +0000

    sprachen, GG,SG

--- midgard/xml/land_sprache_exp.cc
+++ midgard/xml/land_sprache_exp.cc
@@ -1,4 +1,4 @@
-// $Id: land_sprache_exp.cc,v 1.3 2001/12/27 14:47:40 christof Exp $
+// $Id: land_sprache_exp.cc,v 1.4 2001/12/27 21:20:41 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001 Christof Petig
  *
@@ -45,7 +45,7 @@ void land_speichern(std::ostream &o)
 
 //******************************************************************
    o << " <Schriften>\n";
-  {Query query("select name, region, typ, kosten"
+  {Query query("select name, region, typ, kosten, alt, kult"
    	" from schrift"
    	" where coalesce(region,'')='"+region+"'"
    	" order by coalesce(region,''),name");
@@ -55,10 +55,34 @@ void land_speichern(std::ostream &o)
    fetch_and_write_string_attrib(is, o, "Region");
    fetch_and_write_string_attrib(is, o, "Typ");
    fetch_and_write_int_attrib(is, o, "Kosten");
+   fetch_and_write_bool_attrib(is, o, "alte_Schrift");
+   fetch_and_write_bool_attrib(is, o, "Kultschrift");
    o << "/>\n";
   }
   }
    o << " </Schriften>\n";
+
+//******************************************************************
+   o << " <Sprachen>\n";
+  {Query query("select name, region, fp, max_wert, schrift, alt,"
+  	" gruppe_1, gruppe_2, minderheit"
+   	" from schrift"
+   	" where coalesce(region,'')='"+region+"'"
+   	" order by coalesce(region,''),name");
+  while ((query>>is).good())
+  {o << "  <Sprache";
+   fetch_and_write_string_attrib(is, o, "Name");
+   fetch_and_write_string_attrib(is, o, "Region");
+   fetch_and_write_int_attrib(is, o, "Kosten");
+   fetch_and_write_int_attrib(is, o, "Maximalwert");
+   fetch_and_write_string_attrib(is, o, "Schrift");
+   fetch_and_write_int_attrib(is, o, "Gruppe_1"); // ??
+   fetch_and_write_int_attrib(is, o, "Gruppe_2"); // ??
+   fetch_and_write_bool_attrib(is, o, "Minderheit");
+   o << "/>\n";
+  }
+  }
+   o << " </Sprachen>\n";
 
 //******************************************************************
   if (region=="")
