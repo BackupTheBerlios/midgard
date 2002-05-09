0a6aba1ae8d1fb1680bfe8377bf143b1b81e418c
christof <christof>
2002-05-09 16:59:55 +0000

    *** empty log message ***

--- midgard/src/customize_toolbars.cc
+++ midgard/src/customize_toolbars.cc
@@ -1,4 +1,4 @@
-// $Id: customize_toolbars.cc,v 1.1 2002/05/03 10:26:29 christof Exp $
+// $Id: customize_toolbars.cc,v 1.2 2002/05/09 16:59:55 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001-2002 Christof Petig
  *
@@ -23,31 +23,33 @@
 #include <gtk--/pixmap.h>
 #include <gtk--/button.h>
 #include <gtk--/box.h>
+#include <typeinfo>
 // bin + container sind schon dabei
 
 void Gtk::CustomizeToolbars(Gtk::Widget *w, bool show_icons, bool show_text)
 {  if (Gtk::Button::isA(w))
-   {  Gtk::Widget *child=dynamic_cast<Gtk::Bin>(w)->get_child();
+   {  Gtk::Widget *child=dynamic_cast<Gtk::Bin*>(w)->get_child();
       if (Gtk::HBox::isA(child))
-      {  BoxList &ch=ch->dynamic_cast<Gtk::Box>(w)->get_children();
-         BoxList::iterator i=ch.begin(),j=i;
+      {  Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(w)->children();
+         Gtk::Box_Helpers::BoxList::iterator i=ch.begin(),j=i;
          if (i!=ch.end() && ++j!=ch.end())
-         {  if (Gtk::Pixmap::isA(i->get_widget()) && Gtk::Label::isA(j->get_widget()))
-            {  if (show_icons) i->get_widget()->show();
-               else i->get_widget()->hide();
-               if (show_text) j->get_widget()->show();
-               else j->get_widget()->hide();
+         {  if (Gtk::Pixmap::isA((*i)->get_widget()) && Gtk::Label::isA((*j)->get_widget()))
+            {  if (show_icons) (*i)->get_widget()->show();
+               else (*i)->get_widget()->hide();
+               if (show_text) (*j)->get_widget()->show();
+               else (*j)->get_widget()->hide();
             }
          }
       }
    }
    else if (Gtk::Toolbar::isA(w))
-   {  Gtk::Toolbar tb=dynamic_cast<Gtk::Toolbar>(w);
+   {  Gtk::Toolbar *tb=dynamic_cast<Gtk::Toolbar*>(w);
       tb->set_style(show_icons ? (show_text?GTK_TOOLBAR_BOTH:GTK_TOOLBAR_ICONS)
       		:GTK_TOOLBAR_TEXT);
       // recurse ?
    }
    else if (Gtk::Container::isA(w))
    {  // und nun ?
+      cout << typeid(*w).name() << '\n';
    }
 }
