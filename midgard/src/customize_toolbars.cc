492d040543a1ab4d9b625ed90fb1adacd71e06fc
christof <christof>
2002-05-15 08:57:33 +0000

    *** empty log message ***

--- midgard/src/customize_toolbars.cc
+++ midgard/src/customize_toolbars.cc
@@ -1,4 +1,4 @@
-// $Id: customize_toolbars.cc,v 1.5 2002/05/09 17:24:19 christof Exp $
+// $Id: customize_toolbars.cc,v 1.6 2002/05/15 08:57:33 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001-2002 Christof Petig
  *
@@ -24,22 +24,30 @@
 #include <gtk--/pixmap.h>
 #include <gtk--/button.h>
 #include <gtk--/box.h>
+#include <gtk--/notebook.h>
 #include <typeinfo>
 // bin + container sind schon dabei
 
 void Gtk::CustomizeToolbars(Gtk::Widget *w, bool show_icons, bool show_text)
-{  cout << '+' << typeid(*w).name() << '\n';
+{  cout << '+' << typeid(*w).name() << '-' << w->get_name() << '\n';
    if (Gtk::Button::isA(w))
    {  Gtk::Widget *child=dynamic_cast<Gtk::Bin*>(w)->get_child();
-      if (Gtk::HBox::isA(child))
-      {  Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(w)->children();
+      if (child && Gtk::Box::isA(child))
+      {  Gtk::wrap(w);
+         Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(w)->children();
          Gtk::Box_Helpers::BoxList::iterator i=ch.begin(),j=i;
-         if (i!=ch.end() && ++j!=ch.end())
+         if (i!=ch.end() && ++j!=ch.end() && (*i)->get_widget() && (*j)->get_widget())
          {  if (Gtk::Pixmap::isA((*i)->get_widget()) && Gtk::Label::isA((*j)->get_widget()))
             {  if (show_icons) (*i)->get_widget()->show();
                else (*i)->get_widget()->hide();
                if (show_text) (*j)->get_widget()->show();
                else (*j)->get_widget()->hide();
+            }
+            else if (Gtk::Label::isA((*i)->get_widget()) && Gtk::Pixmap::isA((*j)->get_widget()))
+            {  if (show_icons) (*j)->get_widget()->show();
+               else (*j)->get_widget()->hide();
+               if (show_text) (*i)->get_widget()->show();
+               else (*i)->get_widget()->hide();
             }
          }
       }
@@ -52,16 +60,29 @@ void Gtk::CustomizeToolbars(Gtk::Widget *w, bool show_icons, bool show_text)
    }
    else if (Gtk::Bin::isA(w))
    {  Gtk::Widget *child=dynamic_cast<Gtk::Bin*>(w)->get_child();
-      CustomizeToolbars(child,show_icons,show_text);
+      if (child) CustomizeToolbars(child,show_icons,show_text);
    }
    else if (Gtk::Table::isA(w))
    {  
-#if 0   
+#if 0   // a bug in gtkmm-1.2
       Gtk::Table_Helpers::TableList &ch=dynamic_cast<Gtk::Table*>(w)->children();
       for (Gtk::Table_Helpers::TableList::const_iterator i=ch.begin();
       		i!=ch.end();++i)
          CustomizeToolbars(*i,show_icons,show_text);
 #endif
+      // G_ListWrap<GtkTableChild*>
+      for (GList *list=GTK_TABLE(w->gtkobj())->children;list;
+      		list=list->next)
+      {  CustomizeToolbars(Gtk::wrap(((GtkTableChild*)(list->data))->widget),
+      		show_icons,show_text);
+      }
+   }
+   else if (Gtk::Notebook::isA(w))
+   {  Gtk::Notebook_Helpers::PageList &ch=dynamic_cast<Gtk::Notebook*>(w)->pages();
+      for (Gtk::Notebook_Helpers::PageList::const_iterator i=ch.begin();
+      		i!=ch.end();++i)
+         CustomizeToolbars((*i)->get_child(),show_icons,show_text);
+      // what about the tab?
    }
    else if (Gtk::Container::isA(w))
    {  // und nun ?
