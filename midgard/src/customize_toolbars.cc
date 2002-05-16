22415190e08bc1abf928362057be0fd1f678fe40
christof <christof>
2002-05-16 07:41:37 +0000

    *** empty log message ***

--- midgard/src/customize_toolbars.cc
+++ midgard/src/customize_toolbars.cc
@@ -1,4 +1,4 @@
-// $Id: customize_toolbars.cc,v 1.10 2002/05/16 07:36:59 christof Exp $
+// $Id: customize_toolbars.cc,v 1.11 2002/05/16 07:41:37 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001-2002 Christof Petig
  *
@@ -29,59 +29,40 @@
 #include <typeinfo>
 // bin + container sind schon dabei
 
+static void CustomizeBox(Gtk::Widget *child, bool show_icons, bool tab_text)
+{  Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(child)->children();
+   Gtk::Box_Helpers::BoxList::iterator i=ch.begin(),j=i;
+   
+   if (i!=ch.end() && ++j!=ch.end() && (*i)->get_widget() && (*j)->get_widget())
+   {  Gtk::Widget *w1=(*i)->get_widget();
+      Gtk::Widget *w2=(*j)->get_widget();
+      
+      if (Gtk::Pixmap::isA(w1) && Gtk::Label::isA(w2))
+      {  if (show_icons) w1->show();
+         else w1->hide();
+         if (tab_text) w2->show();
+         else w2->hide();
+      }
+      else if (Gtk::Label::isA(w1) && Gtk::Pixmap::isA(w2))
+      {  if (show_icons) w2->show();
+         else w2->hide();
+         if (tab_text) w1->show();
+         else w1->hide();
+      }
+   }
+}
+
 static void CustomizeTab(Gtk::Widget *w, bool show_icons, bool tab_text)
 {  if (Gtk::EventBox::isA(w)) w=dynamic_cast<Gtk::Bin*>(w)->get_child();
    if (!w) return;
 
    Gtk::Widget *child=dynamic_cast<Gtk::Bin*>(w)->get_child();
-   if (child && Gtk::Box::isA(child))
-   {  Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(child)->children();
-      Gtk::Box_Helpers::BoxList::iterator i=ch.begin(),j=i;
-      
-      if (i!=ch.end() && ++j!=ch.end() && (*i)->get_widget() && (*j)->get_widget())
-      {  Gtk::Widget *w1=(*i)->get_widget();
-         Gtk::Widget *w2=(*j)->get_widget();
-         
-         if (Gtk::Pixmap::isA(w1) && Gtk::Label::isA(w2))
-         {  if (show_icons) w1->show();
-            else w1->hide();
-            if (tab_text) w2->show();
-            else w2->hide();
-         }
-         else if (Gtk::Label::isA(w1) && Gtk::Pixmap::isA(w2))
-         {  if (show_icons) w2->show();
-            else w2->hide();
-            if (tab_text) w1->show();
-            else w1->hide();
-         }
-      }
-   }
+   if (child && Gtk::Box::isA(child)) CustomizeBox(child,show_icons,tab_text);
 }
 
 static void CustomizeButton(Gtk::Widget *w, bool show_icons, bool show_text)
 {  Gtk::Widget *child=dynamic_cast<Gtk::Bin*>(w)->get_child();
-   if (child && Gtk::Box::isA(child))
-   {  Gtk::Box_Helpers::BoxList &ch=dynamic_cast<Gtk::Box*>(child)->children();
-      Gtk::Box_Helpers::BoxList::iterator i=ch.begin(),j=i;
-      
-      if (i!=ch.end() && ++j!=ch.end() && (*i)->get_widget() && (*j)->get_widget())
-      {  Gtk::Widget *w1=(*i)->get_widget();
-         Gtk::Widget *w2=(*j)->get_widget();
-         
-         if (Gtk::Pixmap::isA(w1) && Gtk::Label::isA(w2))
-         {  if (show_icons) w1->show();
-            else w1->hide();
-            if (show_text) w2->show();
-            else w2->hide();
-         }
-         else if (Gtk::Label::isA(w1) && Gtk::Pixmap::isA(w2))
-         {  if (show_icons) w2->show();
-            else w2->hide();
-            if (show_text) w1->show();
-            else w1->hide();
-         }
-      }
-   }
+   if (child && Gtk::Box::isA(child)) CustomizeBox(child,show_icons,show_text);
 }
 
 void Gtk::CustomizeToolbars(Gtk::Widget *w, bool show_icons, bool show_text, bool tab_text)
@@ -89,7 +70,7 @@ void Gtk::CustomizeToolbars(Gtk::Widget *w, bool show_icons, bool show_text, boo
    if (!show_icons && !show_text) show_text=true;
    if (!show_icons && !tab_text) tab_text=true;
    if (Gtk::Button::isA(w))
-   {  CustomizeButton(w, bool show_icons, bool show_text);
+   {  CustomizeButton(w, show_icons, show_text);
    }
    else if (Gtk::Toolbar::isA(w))
    {  Gtk::Toolbar *tb=dynamic_cast<Gtk::Toolbar*>(w);
