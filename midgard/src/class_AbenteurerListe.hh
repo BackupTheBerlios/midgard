5286cf910f437e2de3f709aa938d8d4fae13284d
thoma <thoma>
2002-05-28 05:22:32 +0000

    *** empty log message ***

---
+++ midgard/src/class_AbenteurerListe.hh
@@ -0,0 +1,61 @@
+/*  Midgard Character Generator
+ *  Copyright (C) 2002 Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <Aux/EntryValueIntString.h>
+#include <rowdata.h>
+#include <Abenteurer.hh>
+
+class Data_AbenteurerListe : public RowDataBase
+{
+      Abenteurer Char;
+
+      enum spalten{NAMEA,TYP,BEZEICHNUNG,SPEZIES,SPIELER,VER};
+   public:
+      Data_AbenteurerListe(const Abenteurer& c) : Char(c) {}
+
+      virtual const cH_EntryValue Value(guint seqnr,gpointer gp) const
+       {
+         switch(spalten(seqnr))
+          {
+            case NAMEA: return cH_EntryValueIntString(Char.getCWerte().Name_Abenteurer());
+            case SPIELER: return cH_EntryValueIntString(Char.getCWerte().Name_Spieler());
+            case BEZEICHNUNG: return cH_EntryValueIntString(Char.getCWerte().Bezeichnung());
+            case SPEZIES: return cH_EntryValueIntString(Char.getCWerte().Spezies()->Name());
+            case TYP: {
+               std::string s1=Char.CTyp1()->Name(Char.getCWerte().Geschlecht());
+               std::string s2=Char.CTyp2()->Name(Char.getCWerte().Geschlecht());
+               if(s2!="") s1+="/"+s2;
+               return cH_EntryValueIntString(s1);
+              }
+            case VER: return cH_EntryValueIntString(Char.getCWerte().Version());
+          }
+        return cH_EntryValueIntString();
+       }
+   Abenteurer &getAbenteurer() const {return const_cast<Abenteurer&>(Char);}
+};
+
+class cH_Data_AbenteurerListe : public Handle<const Data_AbenteurerListe>
+{
+protected:
+ cH_Data_AbenteurerListe() {}
+ public:
+  cH_Data_AbenteurerListe(const Data_AbenteurerListe *r) : Handle<const Data_AbenteurerListe>(r){}
+};
+  
+
+
