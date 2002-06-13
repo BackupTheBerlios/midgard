7e6770d31fa31d817d7c663924e899323fefb802
christof <christof>
2002-06-13 19:41:13 +0000

    remove unneeded lines

--- midgard/xml/export_common.cc
+++ midgard/xml/export_common.cc
@@ -1,4 +1,4 @@
-// $Id: export_common.cc,v 1.18 2002/06/12 13:54:10 christof Exp $
+// $Id: export_common.cc,v 1.19 2002/06/13 19:41:13 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001-2002 Christof Petig
  *
@@ -26,25 +26,6 @@ std::string region;
 #define NO_SQL
 #endif
 
-#if 0
-static const char HEX[]="0123456789ABCDEF";
-
-std::string toXML(const std::string &s)
-{  std::string res;
-   for (std::string::const_iterator i=s.begin();i!=s.end();++i)
-   {  if (isalnum(*i)) res+=*i;
-      else if ((unsigned char)*i>=160) res+=*i;
-      else if (strchr("# @^+-*/.,?!$'`|~[]{}()_:;=",*i)) res+=*i;
-      else if (*i=='&') res+="&amp;";
-      else if (*i=='<') res+="&lt;";
-      else if (*i=='>') res+="&gt;";
-      else if (*i=='"') res+="&quot;";
-      else res+=std::string("&#x")+HEX[(*i>>4)&0xf]+HEX[*i&0xf]+';';
-   }
-   return res;
-}
-#endif
-
 #ifndef NO_SQL
 std::string toSQL(const std::string &s)
 {  std::string res;
@@ -63,20 +44,6 @@ int fetch_int(FetchIStream &is,int standard=0)
    return val;
 }
 
-#if 0
- int fetch_and_write_int(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0)
-{  int val=fetch_int(is);
-   write_int(o,wert,val,indent);
-   return val;
-}
-
-int fetch_and_write_int_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,int standard=0)
-{  int val=fetch_int(is,standard);
-   write_int_attrib(o,wert,val,standard);
-   return val;
-}
-#endif
-
 int fetch_and_set_int_attrib(FetchIStream &is,Tag &o,const std::string &wert,int standard=0)
 {  int val=fetch_int(is,standard);
    if (val!=standard) o.setIntAttr(wert,val);
@@ -90,40 +57,12 @@ double fetch_float(FetchIStream &is,double standard=0)
    return val;
 }
 
-#if 0
-double fetch_and_write_float(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0)
-{  double val=fetch_float(is);
-   write_float(o,wert,val,indent);
-   return val;
-}
-
-double fetch_and_write_float_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,double standard=0)
-{  double val=fetch_float(is,standard);
-   write_float_attrib(o,wert,val,standard);
-   return val;
-}
-#endif
-
  std::string fetch_string(FetchIStream &is,const std::string &standard="")
 {  std::string val;
    
    is >> FetchIStream::MapNull<string>(val,standard); 
    return val;
 }
-
-#if 0
- std::string fetch_and_write_string(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0)
-{  std::string val=fetch_string(is);
-   write_string(o,wert,val,indent);
-   return val;
-}
-
- std::string fetch_and_write_string_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,const std::string &standard)
-{  std::string val=fetch_string(is,standard);
-   write_string_attrib(o,wert,val,standard);
-   return val;
-}
-#endif
 
  std::string fetch_and_set_string_attrib(FetchIStream &is,Tag &o,const std::string &wert,const std::string &standard)
 {  std::string val=fetch_string(is,standard);
@@ -150,77 +89,5 @@ double fetch_and_set_float_attrib(FetchIStream &is,Tag &o,const std::string &wer
    return val;
 }
 
-#if 0
- bool fetch_and_write_bool_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,bool standard)
-{  bool val=fetch_bool(is,standard);
-   write_bool_attrib(o,wert,val,standard);
-   return val;
-}
-
- std::string typ_standardisierung(const std::string &t)
-{  if (t=="Ord") return "Or";
-   else if (t=="Ass") return "As";
-   else return t;
-}
-
- std::string fetch_typ(FetchIStream &is,const std::string &standard="")
-{  return typ_standardisierung(fetch_string(is,standard));
-}
-
- std::string fetch_and_write_typ_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,const std::string &standard="")
-{  std::string val=fetch_typ(is,standard);
-   write_string_attrib(o,wert,val,standard);
-   return val;
-}
 #endif
 
-#endif
-
-#if 0
- void write_int(std::ostream &o,const std::string &wert,int val, int indent=0)
-{  if (!val) return;
-   for (int i=0;i<indent;++i) o << ' ';
-   o << '<' << wert << '>' << val << "</" << wert << ">\n";
-}
-
- void write_int_attrib(std::ostream &o,const std::string &wert,int val, int standard=0)
-{  if (val==standard) return;
-   write_int_attrib_force(o,wert,val);
-}
-
- void write_int_attrib_force(std::ostream &o,const std::string &wert,int val)
-{  o << ' ' << wert << "=\"" << val << '\"';
-}
-
-void write_float(std::ostream &o,const std::string &wert,double val, int indent=0)
-{  if (!val) return;
-   for (double i=0;i<indent;++i) o << ' ';
-   o << '<' << wert << '>' << val << "</" << wert << ">\n";
-}
-
-void write_float_attrib(std::ostream &o,const std::string &wert,double val, double standard=0)
-{  if (val==standard) return;
-   o << ' ' << wert << "=\"" << val << '\"';
-}
-
- void write_string(std::ostream &o,const std::string &wert,const std::string &val,int indent=0)
-{  if (!val.size()) return;
-   for (int i=0;i<indent;++i) o << ' ';
-   o << '<' << wert << '>' << toXML(val) << "</" << wert << ">\n";
-}
-
- void write_string_attrib(std::ostream &o,const std::string &wert,const std::string &val,const std::string &standard="")
-{  if (val==standard) return;
-   o << ' ' << wert << "=\"" << toXML(val) << '\"';
-}
-
- void write_bool_attrib(std::ostream &o,const std::string &wert,bool val, bool standard=false)
-{  if (val==standard) return;
-   write_bool_attrib_force(o,wert,val);
-}
-
- void write_bool_attrib_force(std::ostream &o,const std::string &wert,bool val)
-{  o << ' ' << wert << "=\"" << (val?"true":"false") << '\"';
-}
-
-#endif
--- midgard/xml/export_common.h
+++ midgard/xml/export_common.h
@@ -1,4 +1,4 @@
-// $Id: export_common.h,v 1.23 2002/06/05 06:43:29 christof Exp $
+// $Id: export_common.h,v 1.24 2002/06/13 19:41:13 christof Exp $
 /*  Midgard Roleplaying Character Generator
  *  Copyright (C) 2001-2002 Christof Petig
  *
@@ -33,52 +33,20 @@ extern std::string region;
 #define IF_REGION(x) x
 #define IF_NOT_REGION(x) 
 
-std::string toXML(const std::string &s);
 std::string toSQL(const std::string &s);
 
 int fetch_int(FetchIStream &is,int standard=0);
-void write_int(std::ostream &o,const std::string &wert,int val, int indent=0);
-void write_int_attrib_force(std::ostream &o,const std::string &wert,int val);
-void write_int_attrib(std::ostream &o,const std::string &wert,int val, int standard=0);
-inline void write_int_attrib_force(std::ostream &o,const std::string &wert,int val);
-int fetch_and_write_int(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0);
-int fetch_and_write_int_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,int standard=0);
 int fetch_and_set_int_attrib(FetchIStream &is,Tag &o,const std::string &wert,int standard=0);
 
 double fetch_float(FetchIStream &is,double standard=0);
-void write_float(std::ostream &o,const std::string &wert,double val, int indent=0);
-void write_float_attrib(std::ostream &o,const std::string &wert,double val, double standard=0);
-double fetch_and_write_float(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0);
-double fetch_and_write_float_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,double standard=0);
 double fetch_and_set_float_attrib(FetchIStream &is,Tag &o,const std::string &wert,double standard=0);
 
 std::string fetch_string(FetchIStream &is,const std::string &standard="");
-void write_string(std::ostream &o,const std::string &wert,const std::string &val,int indent=0);
-void write_string_attrib(std::ostream &o,const std::string &wert,const std::string &val,const std::string &standard="");
-std::string fetch_and_write_string(FetchIStream &is,std::ostream &o,const std::string &wert,int indent=0);
-std::string fetch_and_write_string_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,const std::string &standard="");
 std::string fetch_and_set_string_attrib(FetchIStream &is,Tag &o,const std::string &wert,const std::string &standard="");
 
 bool fetch_bool(FetchIStream &is,const bool &standard=false);
-void write_bool_attrib(std::ostream &o,const std::string &wert,bool val, bool standard=false);
-void write_bool_attrib_force(std::ostream &o,const std::string &wert,bool val);
-bool fetch_and_write_bool_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,bool standard=false);
 bool fetch_and_set_bool_attrib(FetchIStream &is,Tag &o,const std::string &wert,bool standard=false);
 
-std::string typ_standardisierung(const std::string &t);
-std::string fetch_typ(FetchIStream &is,const std::string &standard="");
-std::string fetch_and_write_typ_attrib(FetchIStream &is,std::ostream &o,const std::string &wert,const std::string &standard="");
-
-// common routines for exporting from tables
-#if 0
-void grund_standard_ausnahme(ostream &o, 
-	const std::string &table, const std::string &name,
-	const std::string &condition="", bool nur_region=false);
-void lernschema(ostream &o, const std::string &art, const std::string &name,
-	bool nur_region=false);
-void ausnahmen(ostream &o, const std::string &art, const std::string &name,
-	bool nur_region=false);
-#endif
 void grund_standard_ausnahme(Tag &o, 
 	const std::string &table, const std::string &name,
 	const std::string &condition="", bool nur_region=false);
