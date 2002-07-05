0f0baa5e830786667a3d08c14137ca3001b32d76
christof <christof>
2002-07-05 07:12:09 +0000

    *** empty log message ***

--- midgard-postgres/typvereinfachung/typvereinf.cc
+++ midgard-postgres/typvereinfachung/typvereinf.cc
@@ -27,3 +27,22 @@ Gruppen::Gruppen()
 {  typen=Query("select typ from typen_gruppe where gruppe='"+n+"' order by typ")
 	.FetchArray<std::string>();
 }
+
+enum gsan_t { Grund, Standard, Ausnahme, Nicht, GsanAnz, Illegal=-1 };
+
+struct Schnitt
+{  int gsan[GsanAnz];
+   
+   int Wieviel(gsan_t wo);
+   gsan_t Maximum() const; // gibt wo zurück oder -1
+};
+
+class Fertigkeit
+{  std::vector<std::string> gsan[GsanAnz];
+   bool nicht_notwendig;
+   std::vector<std::string> &get_vec(gsan_t wo) { return gsan[wo]; }
+public:
+   Fertigkeit(const std::string &name);
+   Schnitt schneiden(const Gruppe &g) const;
+   void ersetze(gsan_t wo, const Gruppe &g);
+};
