79fa543b988188bf6f9c9f83748bd56738baa0ab
christof <christof>
2003-09-01 16:50:30 +0000

    MagusDialog

---
+++ midgard/src/MagusDialog.cc
@@ -0,0 +1,13 @@
+// generated 2003/9/1 18:46:14 CEST by christof@puck.petig-baender.de
+// using glademm V2.0.0c_cvs
+//
+// newer (non customized) versions of this file go to MagusDialog.cc_new
+
+// This file is for your program, I won't touch it again!
+
+#include "config.h"
+#include "MagusDialog.hh"
+
+bool MagusDialog::on_WindowInfo_delete_event(GdkEventAny *ev)
+{  return 0;
+}
---
+++ midgard/src/MagusDialog.hh
@@ -0,0 +1,20 @@
+// generated 2003/9/1 18:46:14 CEST by christof@puck.petig-baender.de
+// using glademm V2.0.0c_cvs
+//
+// newer (non customized) versions of this file go to MagusDialog.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _MAGUSDIALOG_HH
+#  include "MagusDialog_glade.hh"
+#  define _MAGUSDIALOG_HH
+class MagusDialog : public MagusDialog_glade
+{  
+        
+        bool on_WindowInfo_delete_event(GdkEventAny *ev);
+};
+#endif
